073ac427f13ed98c3a4c6b0117047febdc5c8952
pedramrabiee <pedram.rabiee@gmail.com>
2024-05-19 11:17:21 -0400

    Fix map configs

--- obstacle_rich_env/configs/map_configs.py
+++ obstacle_rich_env/configs/map_configs.py
@@ -2,10 +2,9 @@ from attrdict import AttrDict as AD
 
 map_config_unicycle = AD(softmax_rho=20,
                          softmin_rho=20,
-                         pos_barrier_rel_deg=3,
-                         vel_barrier_rel_deg=2,
-                         obstacle_alpha=[1.0, 2.5],
-                         boundary_alpha=[6.0, 1.0],
-                         velocity_alpha=[10.0],
+                         pos_barrier_rel_deg=2,
+                         vel_barrier_rel_deg=1,
+                         obstacle_alpha=[2.5],
+                         boundary_alpha=[1.0],
+                         velocity_alpha=[],
                          )
-
